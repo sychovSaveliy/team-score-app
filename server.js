0396833e6363a10fe892c18ed7cdabbaae7dc668
SychovSaveliy <sychov.saveliy@gmail.com>
2019-04-09 01:46:54 +0300

    init

--- server.js
+++ server.js
@@ -1,28 +1,8 @@
-let express = require('express');
-let app = express();
-let bodyParser = require('body-parser');
-let $data = require('./routes/data');
-let port = 5000;
-
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(bodyParser.text());
-app.use(bodyParser.json({type: 'application/json'}));
-app.use(function (req, res, next) {
-	res.setHeader('Access-Control-Allow-Origin', '*');
-	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-	res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-	res.setHeader('Access-Control-Allow-Credentials', true);
-	next();
-});
-
-app.get('/', function (req, res) {
-	return res.json({ message: 'UI API!' });
-});
-
-app.route('*')
-	.get($data.mainReq);
-
-app.listen(port);
-console.log('Mock server listening on port ' + port);
-
-module.exports = { app };
+var express = require('express')
+var path = require('path')
+var serveStatic = require('serve-static')
+app = express()
+app.use(serveStatic(__dirname))
+var port = process.env.PORT || 5000
+app.listen(port)
+console.log('server started ' + port)
