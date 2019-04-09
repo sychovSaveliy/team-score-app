38589ccf98471ce674df76e060df5ff2fcf32966
savelii.sychov <savelii.sychov@myheritage.com>
2019-04-09 19:42:43 +0300

    add btn

--- src/views/prototype/Prototype-1.vue
+++ src/views/prototype/Prototype-1.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="prototype">
     {{ text }}
+
+    <button class="btn" @click="btnClick">Click</button>
   </div>
 </template>
 
@@ -11,12 +13,22 @@ export default {
     return {
       text: "Example text prototype 1"
     };
+  },
+  methods: {
+    btnClick() {
+      console.log('click')
+    }
   }
 };
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
 .prototype {
   font-size: 24px;
+}
+
+.btn {
+  font-size: 24px;
+  padding: 5px;
 }
 </style>
