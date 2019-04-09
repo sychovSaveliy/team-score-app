fb3a590fd9030784b0bbd29229d83aff0f0e0687
olga <olga.garanko@gmail.com>
2019-04-09 17:36:54 +0300

    isMobile

--- src/App.vue
+++ src/App.vue
@@ -5,10 +5,47 @@
       <router-link to="/about">About</router-link> |
       <router-link to="/prototype-1">Prot 1</router-link>
     </div> -->
-    <router-view />
+    <div id="content" v-if="mobile">
+      <router-view />
+    </div>
+    <div id="error" v-else>
+      Error - Not mobile
+    </div>
   </div>
 </template>
-
+<script>
+  export default {
+    name: 'App',
+    data () {
+      return {
+        'mobile': false,
+      }
+    },
+    created () {
+      this.isMobile();
+    },
+    methods: {
+      onResize(event) {
+        console.log('window has been resized', event)
+      }
+    },
+    mounted() {
+      window.addEventListener('resize', this.isMobile)
+    },
+    beforeDestroy() {
+      window.removeEventListener('resize', this.isMobile)
+    },
+    methods: {
+      isMobile () {
+        if (window.matchMedia("(min-width: 768px)").matches) {
+          this.mobile = false
+        } else {
+          this.mobile = true
+        }
+      },
+    },
+  }
+</script>
 <style lang="scss">
 #app {
   font-family: "Avenir", Helvetica, Arial, sans-serif;
