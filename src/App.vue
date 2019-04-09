931b809d76db6dd65c1feff7342bfe8ce1dd01be
savelii.sychov <savelii.sychov@myheritage.com>
2019-04-09 23:58:31 +0300

    styles for error component moved to component

--- src/App.vue
+++ src/App.vue
@@ -49,42 +49,4 @@ body {
   color: #2c3e50;
   height: 100%;
 }
-
-#error {
-  position: fixed;
-  left: 0;
-  top: 0;
-  width: 100%;
-  height: 100%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-
-  .error-content {
-    background-color: rgb(133, 0, 0);
-    color: #fafafa;
-    padding: 15px;
-    font-size: 24px;
-    z-index: 2;
-    position: relative;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    border-radius: 15px;
-    cursor: pointer;
-    width: $error-width;
-    height: $error-height;
-    user-select: none;
-  }
-
-  .bg-box {
-    position: absolute;
-    left: 0;
-    top: 0;
-    width: 100%;
-    height: 100%;
-    background-color: rgba(0, 0, 0, 0.8);
-    z-index: 1;
-  }
-}
 </style>
