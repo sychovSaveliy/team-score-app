a536e2a4852668909f22ed996c86ac38e94b1fc5
helga-hai <helga-hai@ukr.net>
2019-04-27 23:24:52 +0300

    Tlabel

---
+++ src/components/Tlabel.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="label" :txt="txt" :team-events="team.data.events">{{ txt }}</div>
+</template>
+
+<script>
+export default {
+  name: "Tlabel",
+  props: {
+    txt: String,
+    team: {default: {data: {name: ''}}, type: Object},
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+$color: #0E72CF;
+
+.label {
+  display:inline-block;
+  padding:0 12px;
+  font-size: 16px;
+  line-height: 27px;
+  position: relative;
+  text-transform:uppercase;
+  height: 26px;
+  text-align: center;
+  color: $color;
+  letter-spacing: 0.4px;
+  background: #F5F5F5;
+  border-radius: 20px;
+}
+
+</style>
--- src/views/prototype/Prototype-5.vue
+++ src/views/prototype/Prototype-5.vue
@@ -1,26 +1,74 @@
 <template>
   <div class="prototype">
     <Ttabs :list='list' />
+    <br>
+    <hr>
+    <br>
+    <Tlabel :txt="txt"></Tlabel>
+    <br>
+    <br>
+    <hr>
+    <br>
+    <br>
+    <div class="teams">
+      <Tlabel :txt="txt" :team-events="events"></Tlabel>
+      <TeamProfile :team="team" align="right"/>
+    </div>
+    <br>
+    <br>
+    <div class="teams">
+      <TeamProfile :team="team" align="left"/>
+      <Tlabel :txt="txt" :team-events="events"></Tlabel>
+    </div>
+
+
   </div>
 </template>
 
 <script>
 import Ttabs from "@/components/Ttabs";
+import Tlabel from "@/components/Tlabel";
+
+import API from "@/services/ApiService";
+import TeamProfile from "@/components/TeamProfile";
+import TeamLogo from "@/components/TeamLogo";
+
 export default {
   name: "Prototype5",
   components: {
-    Ttabs
+    Ttabs,
+    Tlabel,
+    TeamProfile,
+    TeamLogo
   },
   data() {
     return {
-      text: "Example text prototype 5",
+      txt: "тренировка",
       list: [
         { title: "мои игры", actionTab: this.showMyGames, classNames: ["active"] },
         { title: "все игры", actionTab: this.showAllGames, classNames: [] }
       ],
-      item: ["item"]
+      item: ["item"],
+
+      id: "12",/**/
+      align: "",
+      teams: [],
+      team: {
+        data: {
+          name: "",
+          city: "",
+          events: ['training']
+        }
+      },
+      baseUrl: "/team/"/**/
     };
   },
+
+  created() {/**/
+    let url = this.baseUrl + this.id;
+    this.getData(url);
+    this.getDataAll(this.baseUrl);
+  },/**/
   methods: {
     showMyGames(tabItem) {
       tabItem.classNames = ["active"];
@@ -30,13 +78,40 @@ export default {
     showAllGames(tabItem) {;
       tabItem.classNames = ["active"];
       console.log('do showAllGames');
-    }
-  }
+    },
+    getDataAll(url) {/**/
+      API.fetch(url)
+        .then(data => {
+          this.teams = data;
+        })
+        .catch(function(ex) {
+          console.log("fetch data failed", ex);
+        });
+    },
+    getData(url) {
+      API.fetch(url)
+        .then(data => {
+          this.team = data;
+        })
+        .catch(function(ex) {
+          console.log("fetch data failed", ex);
+        });
+    }/**/
+
+  },
 };
 </script>
 
 <style lang="scss">
-.prototype {
-  font-size: 24px;
+
+.teams {
+  display: flex;
+  flex-wrap: wrap;
+  width: 100%;
+  justify-content: space-around;
+  &__item {
+    flex: 0 1 50%;
+    padding: 0 10px;
+  }
 }
 </style>
