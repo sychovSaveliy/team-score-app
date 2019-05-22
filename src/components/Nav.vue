38b6cb0516a6fbed43119e19f228bf8fb8fc6822
SychovSaveliy <sychov.saveliy@gmail.com>
2019-05-22 20:10:46 +0300

    nav style fix

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,29 +1,40 @@
 <template>
-    <nav class="nav">
-      <ul class="nav__list">
-        <li class="nav__link"><router-link to="/" exact>Главная</router-link></li>
-        <li class="nav__link"><router-link to="/score">Рейтинг</router-link></li>
-        <li class="nav__link"><router-link to="/profile">Профиль</router-link></li>
-        <li class="nav__link"><router-link to="/events">Игры</router-link></li>
-        <li class="nav__link"><router-link to="/search">Поиск</router-link></li>
-        <li class="nav__link"><router-link to="/messages">Сообщения</router-link></li>
-      </ul>
-    </nav>
+  <nav class="nav">
+    <ul class="nav__list">
+      <li class="nav__link">
+        <router-link to="/" exact>Главная</router-link>
+      </li>
+      <li class="nav__link">
+        <router-link to="/score">Рейтинг</router-link>
+      </li>
+      <li class="nav__link">
+        <router-link to="/profile">Профиль</router-link>
+      </li>
+      <li class="nav__link">
+        <router-link to="/events">Игры</router-link>
+      </li>
+      <li class="nav__link">
+        <router-link to="/search">Поиск</router-link>
+      </li>
+      <li class="nav__link">
+        <router-link to="/messages">Сообщения</router-link>
+      </li>
+    </ul>
+  </nav>
 </template>
 
 <script>
 export default {
   name: "Nav",
-  data () {
-    return {
-    }
-  },
+  data() {
+    return {};
+  }
 };
 </script>
 
 <style lang="scss">
-.nav{
-  &__list{
+.nav {
+  &__list {
     margin: 0;
     padding: 0;
     list-style: none;
@@ -32,8 +43,9 @@ export default {
     justify-content: center;
     align-items: center;
   }
-  &__link{
+  &__link {
     padding: 0 10px;
+    font-size: 8px;
   }
 }
 </style>
