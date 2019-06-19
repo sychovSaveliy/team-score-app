b0c995df758aff2fc0edd9457b6ce3af8f21ec7f
helga-hai <helga-hai@ukr.net>
2019-06-19 07:21:55 +0300

    Team Profile Avatar-TPavatar

--- src/views/prototype/Prototype-3.vue
+++ src/views/prototype/Prototype-3.vue
@@ -1,22 +1,72 @@
 <template>
-  <div class="prototype">
-    {{ text }}
+  <div class="team__profile" :class="align">
+    <div class="team__logo">
+      <TPavatar :team-name="team.data.teamName" /><!-- align="left" -->
+      <!--TeamLogo :team-name="team.data.teamName" :align="align"/-->
+    </div>
+    <div class="team__info">
+      <div class="team__name">{{ team.data.teamName }}</div>
+      <div class="team__city">{{ team.data.city }}</div>
+    </div>
   </div>
 </template>
 
 <script>
+import TPavatar from "@/components/TPavatar";
+import TeamLogo from "@/components/TeamLogo";
 export default {
-  name: "Prototype3",
+  name: "TeamProfile",
+  components: {
+    TeamLogo,
+    TPavatar
+  },
   data() {
-    return {
-      text: "Example text prototype 3"
-    };
+    return {};
+  },
+  props: {
+    team: {
+      type: Object,
+      default: function() {
+        return { data: { name: "" } };
+      }
+    },
+    align: { default: "left", type: String }
   }
 };
 </script>
 
-<style lang="scss">
-.prototype {
-  font-size: 24px;
+<style lang="scss" scoped>
+.team {
+  &__profile {
+    color: red;
+    display: flex;
+    align-items: center;
+    justify-content: flex-start;
+    color: rgba(0, 0, 0, 0.87);
+    font-size: 12px;
+    margin-bottom: 10px;
+    &.left {
+      .team__info {
+        text-align: right;
+        margin-right: 10px;
+        margin-left: 0;
+      }
+      .team__logo {
+        order: 1;
+      }
+    }
+  }
+  &__info {
+    flex-grow: 1;
+    text-align: left;
+    margin-left: 10px;
+    margin-right: 0;
+  }
+  &__logo {
+    order: 0;
+  }
+  &__name {
+    font-weight: bold;
+  }
 }
 </style>
